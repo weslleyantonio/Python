b9f422ef7a8152fdbb4f124c2705f89d146cdfb0
weslleyantonio <weslley.miranda@fatec.sp.gov.br>
2019-02-18 07:50:46 -0300

    Add exercise 1006 from uri

---
+++ URI/1006/1006.py
@@ -0,0 +1,11 @@
+def main():
+    A = input()
+    B = input()
+    C = input()
+    A = float(A) * 2
+    B = float(B) * 3
+    C = float(C) * 5
+    media = float(A) + float(B) + float(C)
+    media = media / 10
+    print("MEDIA = {0:.1f}".format(media))
+main()
